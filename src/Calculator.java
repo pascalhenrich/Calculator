ca7527a882ad23899a6343b9089247d19a42855c
Pascal Henrich <pascalh.1505@gmail.com>
2019-04-08 16:53:02 +0200

    First design made!

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,24 +1,24 @@
 import javax.swing.*;
 
 public class Calculator {
-    private JButton button_7;
-    private JButton button_8;
-    private JButton button_9;
-    private JButton button_multiply;
+    private JPanel calculatorPanel;
+    private JButton button_0;
+    private JButton button_1;
+    private JButton button_2;
+    private JButton button_3;
     private JButton button_4;
     private JButton button_5;
     private JButton button_6;
-    private JButton button_subtraction;
-    private JButton button_addition;
-    private JButton button_3;
-    private JButton button_2;
-    private JButton button_1;
-    private JButton button_0;
+    private JButton button_7;
+    private JButton button_8;
+    private JButton button_9;
     private JButton button_point;
-    private JButton button_result;
+    private JButton button_addition;
+    private JButton button_subtraction;
+    private JButton button_multiply;
     private JButton button_division;
+    private JButton button_result;
     private JTextField textField;
-    private JPanel calculatorPanel;
     private JButton button_C;
 
     public static void main(String[] args) {
